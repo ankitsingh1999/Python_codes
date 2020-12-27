a30e039df8e87b7067fdefad6f9941c3cbeef8bb
Ankit Singh <57299163+ankitsingh1999@users.noreply.github.com>
2020-12-27 11:04:15 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,17 @@
+#variables
+a = "hey there how is going"
+print(a)
+
+#so basically variables are somethings which are use to store the data
+
+#turtle
+
+import turtle
+ankit_turtle = turtle.Turtle()
+ankit_turtle.forward(100)
+ankit_turtle.left(90)
+ankit_turtle.forward(100)
+ankit_turtle.left(90)
+ankit_turtle.forward(100)
+ankit_turtle.left(90)
+ankit_turtle.forward(100)
